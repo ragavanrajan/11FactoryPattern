ee349c60e59fa820ad31b1abe49c7ef50edc0055
Ragavan Rajan <rr100@students.waikato.ac.nz>
2018-09-06 13:33:01 +1200

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+function MemberFactory() {
+    this.createMember = function(name, type) {
+        let memeber;
+
+        if (type === 'simple') {
+            member = new SimpleMemberShip(name);
+
+        } else if (type === 'standard') {
+            member = new StandardMemberShip(name);
+        } else if (type === 'super') {
+            member = new SuperMemberShip(name);
+        }
+
+        member.type = type;
+
+        member.define = function() {
+            console.log(`${this.name} (${this.type}): ${this.cost}`);
+
+        }
+
+        return member;
+    }
+}
+
+const SimpleMemberShip = function(name) {
+    this.name = name;
+    this.cost = '$5';
+}
+
+const StandardMemberShip = function(name) {
+    this.name = name;
+    this.cost = '$15';
+}
+
+const SuperMemberShip = function(name) {
+    this.name = name;
+    this.cost = '$25';
+}
+
+const members = [];
+const factory = new MemberFactory();
+
+members.push(factory.createMember('John Domingo', 'simple'));
+members.push(factory.createMember('Mike Bisset', 'super'));
+members.push(factory.createMember('Janice  william,', 'standard'));
+members.push(factory.createMember('John Giblin', 'simple'));
+// console.log(members);
+
+members.forEach(function(member) {
+    member.define();
+})
